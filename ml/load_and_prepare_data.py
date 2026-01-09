1d08784d75b9da4b7b97404afa0a78104af99df0
Parth Parmar <parth3rdeye@gmail.com>
2026-01-09 20:21:14 +0530

    Make features and ml directories Python packages for clean imports

---
+++ ml/load_and_prepare_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from features.feature_engineering import engineer_features
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+
+CSV_PATH = BASE_DIR / "data" / "raw" /"employees.csv"
+
+
+
+df = pd.read_csv(CSV_PATH)
+
+# print(df.head())
+# print(df.columns)
+
+df_features = engineer_features(df)
+
+print(df_features)
